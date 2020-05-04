15c40693327ce36828d4f0976e00dcf6d80c2662
Jagadish Bapanapally <54756958+jagadishb09@users.noreply.github.com>
2020-05-03 22:56:47 -0600

    extracting text from sections

--- classfile.py
+++ classfile.py
@@ -20,7 +20,7 @@ class Access10K:
         with open('masteridx.txt', 'wb') as file:
             file.write(content)
 
-        with open('masteridx.txt', 'r') as file:
+        with open('masteridx.txt', 'r', encoding = "ISO-8859-1") as file:
             startline=0
             line = file.readline()
             m = None
@@ -42,12 +42,33 @@ class Access10K:
                         lastappend = split10k[3]
                         split10k[3] = split10k[3].replace("-","")
                         split10k[3] = split10k[3].replace(".txt\n","")
-                        list10k.append(url1+'/'+split10k[3]+'/'+lastappend)
+                        list10k.append([url1+'/'+split10k[3]+'/'+lastappend, parts[3]])
                     line = file.readline()
             return list10k
 
-    def ciksdateslinks(self, hreflink):
-        #get all ciks with no duplicates and return one cik
+    def sectioninfo(self, link):
+        print(link)
+        r = requests.get(link)
+        raw_10k = r.text
+        doc_start_pattern = re.compile(r'<DOCUMENT>')
+        doc_end_pattern = re.compile(r'</DOCUMENT>')
+        type_pattern = re.compile(r'<TYPE>[^\n]+')
+        doc_start_is = [x.end() for x in doc_start_pattern.finditer(raw_10k)]
+        doc_end_is = [x.start() for x in doc_end_pattern.finditer(raw_10k)]
+        doc_types = [x[len('<TYPE>'):] for x in type_pattern.findall(raw_10k)]
+        document = {}
+
+        # Create a loop to go through each section type and save only the 10-K section in the dictionary
+        for doc_type, doc_start, doc_end in zip(doc_types, doc_start_is, doc_end_is):
+            if doc_type == '10-K':
+                document[doc_type] = raw_10k[doc_start:doc_end]
+
+        regex = re.compile(r'(>Item(\s|&#160;|&nbsp;)(1A)\.{0,1})|(ITEM\s(1A))')
+        matches = regex.finditer(document['10-K'])
+
+        # Write a for loop to print the matches
+        for match in matches:
+            print(match)
         return 'hello world'
 
     def get10klinksdates(self, cik):
