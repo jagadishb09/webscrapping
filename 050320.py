9ce28d364d0b660d3ba663436bdfad3304f2eb59
Jagadish Bapanapally <54756958+jagadishb09@users.noreply.github.com>
2020-05-19 18:23:13 -0600

    final code with comments

--- 050320.py
+++ 050320.py
@@ -9,60 +9,39 @@ import codecs
 import sys
 from classfile1 import Access10K
 reqfuncs = Access10K()
-year = 2011
 qutr = ["QTR1", "QTR2", "QTR3", "QTR4"]
 import xlwt
 from xlwt import Workbook
 wb = Workbook()
 cnt=1
-#for qtr in qutr:
+
 
 for cik in reqfuncs.ciks:
     year=2011
-    print('cik ='+str(cik))
-    #sheet1 = wb.add_sheet(str(cik))
+
+    sheet1 = wb.add_sheet(str(cik))
     while year<2020:
-        print()
-        print()
+
         for qtr in qutr:
             list10ks = reqfuncs.downloadmasteridx(year,qtr, cik)
             for k10 in list10ks:
                 print(k10[0])
-                #sheet1.write(cnt,0, cik)
-                #sheet1.write(cnt,1,str(year)+qtr)
-                #sheet1.write(cnt,2,k10[1])
-                #sheet1.write(cnt,3,k10[0])
+
                 sectinfo = reqfuncs.sectioninfo(k10[0])
-                print(len(sectinfo))
-                print(sectinfo)
-                #sys.exit()
-                cnt = cnt+1
+                for secinfo in sectinfo:
+
+                    sheet1.write(cnt,0, cik)
+                    sheet1.write(cnt,1,str(year)+qtr)
+                    sheet1.write(cnt,2,k10[1])
+                    sheet1.write(cnt,3,k10[0])
+                    sheet1.write(cnt,4, secinfo[0])
+                    sheet1.write(cnt, 5, secinfo[1])
+                    sheet1.write(cnt, 6, secinfo[2])
+                    cnt = cnt + 1
 
         year = year+1
 
-#wb.save('white1.xls')
-    #for each 10k
-    #print(reqfuncs)
-    #for cik in reqfuncs.ciks:
-
-        #get 10-K's list and dates filed for the cik in the file
-        # for each 10-K:
-        #{
-            # get number of sections
-            # for each section
-            #{
-                # get number of words
-                # get readability
-                # get number of headings
-                # for each heading
-                #{
-                    # get number of words
-                    # get readability
-                #}
-            #}
-        #}
-    #delete file master idx file
-
+wb.save('white1.xls')
 
 
 
--- classfile1.py
+++ classfile1.py
@@ -6,22 +6,10 @@ from bs4 import BeautifulSoup
 
 class Access10K:
     def __init__(self):
+        #add all ciks to this list like [51143, 10274, 320193]
         self.ciks=[51143]
-        #1018724
-            #[51143]
-        #320193
-        self.index=0
-    def getnextcik(self):
-        self.index = self.index+1
-        return self.ciks[self.index-1]
 
-    def search_for_bolded_tags(self, tag):
-        criteria1 = tag.name == 'b'
-        criteria2 = tag.parent.name != 'td'
-
-        if criteria1:
-            return tag.get_text(strip = True).replace('\n',' ')
-
+    #function to get the tags with bold style in the html
     def bold_only(self,tag):
         is_b = tag.name == 'b'
         is_strong = tag.name == 'strong'
@@ -29,8 +17,9 @@ class Access10K:
 
         return is_b or is_strong or is_bold_font
 
+    #function to get section info from the text which contains html
+    #gets number of sections, number of words in each section and number of subsections in each section
     def sectioninfo(self, link):
-        #print('sectioninfo')
         r = requests.get(link)
         raw_10k = r.text
         doc_start_pattern = re.compile(r'<DOCUMENT>')
@@ -46,44 +35,31 @@ class Access10K:
             if doc_type == '10-K':
                 document[doc_type] = raw_10k[doc_start:doc_end]
 
-
-
         html = document['10-K']
         soup = BeautifulSoup(html, "html.parser")
         document = str(soup)
-        with open('10k.txt', 'wt') as file:
-            file.write(document)
-
-
+        #with open('10k.txt', 'wt') as file:
+            #file.write(document)
 
         bold=[bold for bold in soup.find_all(self.bold_only)]
-        print(bold)
-        regex = re.compile(
-            r'(Item(\s|&#160;|&nbsp;)([+-]?\d+(?:\.\d+)?)\.{1})|(ITEM\s([+-]?\d+(?:\.\d+)?))')
 
-#<b>Reference<br/>
-            #r'(>Item(\s|&#160;|&nbsp;)(1|1A|1B|2|3|4|5|6|7|7A|8|9|9A|9B|10|11|12|13|14|15)\.{0,1})|(ITEM\s(1|1A|1B|2|3|4|5|6|7|7A|8|9|9A|9B|10|11|12|13|14|15))')
+        regex = re.compile(
+            r'(Item(\s|&#160;|&nbsp;)([+-]?\d+(?:\.\d+)?)([a-z]|[A-Z])*\.{1})|(ITEM\s([+-]?\d+(?:\.\d+)?)\.{1}([a-z]|[A-Z])*\.{1})')
+
         indices=[]
         for x in bold:
             matches=[]
-            matches=regex.findall(str(x))
+            matches=regex.findall(x.get_text())
             if(len(matches) > 0):
-                #print(x)
-                #regex = re.compile(str(x))
-                #print(document.index(str(x)))
-                indices.append([x,document.index(str(x))])
-                #print('breakkkkkkkkkkkkkkkkkkkkkkkkkk')
-        #print(indices)
+                indices.append([x,document.index(str(x)), x.get_text()])
+
         sectioninfo=[]
-        #numsubsecs = []
-        #numwords = []
         for index in range(len(indices) - 1):
             item_1a_raw = document[indices[index][1]:indices[index+1][1]]
-            # print(item_1a_raw)
             item_1a_content = BeautifulSoup(item_1a_raw, 'lxml')
             numwords = len(item_1a_content.get_text("\n\n"))
-            bTags = []
 
+            bTags = []
             for i in item_1a_content.findAll(self.bold_only):
                 tagtd = True
                 for j in i.parents:
@@ -93,149 +69,31 @@ class Access10K:
                 if tagtd:
                     bTags.append(i.text)
 
-                # if(i.parent.name == 'p' and i.parents.name != 'td'):
-                #   bTags.append(i.text)
             numsubsecs = len(bTags)
-            sectioninfo.append([numwords,numsubsecs])
-        #numsubsecs.append(0)
-        #numwords.append(0)
-        sectioninfo.append([0,0])
-        #sectioninfor.append([numsubsecs,numwords])
+            sectioninfo.append([numwords,numsubsecs, indices[index][2]])
+
+        item_1a_raw = document[indices[len(indices)-1][1]:]
+        item_1a_content = BeautifulSoup(item_1a_raw, 'lxml')
+        numwords = len(item_1a_content.get_text("\n\n"))
+
+        bTags = []
+        for i in item_1a_content.findAll(self.bold_only):
+            tagtd = True
+            for j in i.parents:
+                if j.name == 'td':
+                    tagtd = False
+
+            if tagtd:
+                bTags.append(i.text)
+
+        numsubsecs = len(bTags)
+        sectioninfo.append([numwords,numsubsecs,indices[len(indices)-1][2]])
+
         return sectioninfo
-        #print(numsubsecs)
-        #print(numwords)
-        #test_df['numwords'] = numwords
-        #print(test_df)
-        # print(item_1a_content)
-        # print('jdhgfjsdfjhdsddddddddddddddddddddddddddddddd')
-        # data = [b.string for b in item_1a_content.findAll('b')]
-        # print(data)
-        # bold_texts = item_1a_content.find_all(self.search_for_bolded_tags)
-        # print(bold_texts)
-        # print(item_1a_content)
-        # print(item_1a_content.get_text("\n\n"))
-        #sys.exit()
 
-        #sys.exit()
-        '''
-
-        #regex = re.compile(r'(>Item(\s|&#160;|&nbsp;)(1|1A|1B|2|3|4|5|6|7|7A|8|9|9A|9B|10|11|12|13|14|15)\.{0,1})|(ITEM\s(1|1A|1B|2|3|4|5|6|7|7A|8|9|9A|9B|10|11|12|13|14|15))')
-        regex = re.compile(
-            r'(>Item(\s|&#160;|&nbsp;)(1A|1B|7A|7|8)\.{0,1})|(ITEM\s(1A|1B|7A|7|8))')
-        #regex = re.compile(r'(>Item(\s|&#160;|&nbsp;)(5|6|7A|7)\.{0,1})|(ITEM\s(5|6|7A|7))')
-        #regex = re.compile(r'(>Item(\s|&#160;|&nbsp;)([+-]?\d+(?:\.\d+)?)\.{0,1})|(ITEM\s)([+-]?\d+(?:\.\d+)?)')
-        docstr = str(document['10-K'])
-        matches = regex.finditer(docstr)
-
-        test_df = pd.DataFrame([(x.group(), x.start(), x.end()) for x in matches])
-        test_df.columns = ['item', 'start', 'end']
-        test_df['item'] = test_df.item.str.lower()
-        test_df.replace('&#160;', ' ', regex=True, inplace=True)
-        test_df.replace('&nbsp;', ' ', regex=True, inplace=True)
-        test_df.replace(' ', '', regex=True, inplace=True)
-        test_df.replace('\.', '', regex=True, inplace=True)
-        test_df.replace('>', '', regex=True, inplace=True)
-        print(test_df)
-        print('righttttttttttttttttttttttt')
-        df = self.removedups(docstr,test_df)
-        print(df)
-        #test_df=test_df.reset_index()
-        test_df = test_df.reset_index(drop = True)
-        sys.exit()
-        #test_df.set_index('item', inplace=True)
-        #test_df.set_index('item', inplace=True)
-        #print(str(document['10-K']))
-        #doc10k= str(document['10-K'])
-        #print(doc10k)
-        #print (doc10k[410500:410600])
-
-        #print(item_1a_raw)
-        #item_1a_raw = document['10-K'][test_df['start'].loc[0]:test_df['start'].loc[1]]
-
-        numsubsecs=[]
-        numwords = []
-        for i in range(len(test_df)-1):
-            item_1a_raw = document['10-K'][test_df['start'].loc[i]:test_df['start'].loc[i+1]]
-            #print(item_1a_raw)
-            item_1a_content = BeautifulSoup(item_1a_raw, 'lxml')
-            numwords.append(len(item_1a_content.get_text("\n\n")))
-            bTags = []
-
-            for i in item_1a_content.findAll('b'):
-                tagtd = True
-                for j in i.parents:
-                    if j.name == 'td':
-                        tagtd = False
-
-                if tagtd:
-                    bTags.append(i.text)
-
-                #if(i.parent.name == 'p' and i.parents.name != 'td'):
-                 #   bTags.append(i.text)
-            numsubsecs.append(len(bTags))
-        numsubsecs.append(0)
-        numwords.append(0)
-        test_df['numsubsecs'] = numsubsecs
-        test_df['numwords'] = numwords
-        print(test_df)
-            #print(item_1a_content)
-            #print('jdhgfjsdfjhdsddddddddddddddddddddddddddddddd')
-            #data = [b.string for b in item_1a_content.findAll('b')]
-            #print(data)
-            #bold_texts = item_1a_content.find_all(self.search_for_bolded_tags)
-            #print(bold_texts)
-            #print(item_1a_content)
-            #print(item_1a_content.get_text("\n\n"))
-        sys.exit()
-        '''
-    def removedups(self, doc, testdf):
-        #print('dfsjdhfjhsdgfksdgfdksgf')
-        df=[]
-        doc=doc.lower()
-        print(doc)
-        try:
-            result = doc.index('<b>')
-            print(result)
-
-            stack=[]
-            stack.append(['<b>', result])
-            result = result + 2
-            while True:
-                try:
-                    result1 = doc.index('</b>', result)
-                    print(result1)
-                    #result1 = result1+2
-                    try:
-                        resulte = doc.index('<b>',result)
-                        #result = resulte+2
-                        if(result1<resulte):
-                            print('yesssssssssssssssssssssssssssssssssssss')
-                            for index, row in testdf.head().iterrows():
-                                if row['start'] >= stack[-1][1] and row['start'] <= result1:
-                                    df.append(row['start'])
-
-                            stack.pop()
-                            result = result1+2
-                        else:
-                            stack.append(['<b>', resulte])
-                            result = resulte+2
-                    except:
-                        print('yesssssssssssssssssssssssssssssssssssss')
-                        for index, row in testdf.head().iterrows():
-                            if row['start'] >= stack[- 1][1] and row['start'] <= result1:
-                                df.append(row['start'])
-                        stack.pop()
-                        result = result1+2
-
-                except:
-                    return df
-
-
-        except:
-            return df
-
+    #gets master index file for a given year,quarter and cik
     def downloadmasteridx(self, year, qtr, cik):
-        #print('downloadmstridx' + str(cik))
+
         base_url = r"https://www.sec.gov/Archives/edgar/full-index"
         url = '{}/{}'.format(base_url,year)
         url = '{}/{}'.format(url,qtr)
@@ -270,24 +128,3 @@ class Access10K:
                         list10k.append([url1+'/'+split10k[3]+'/'+lastappend, parts[3]])
                     line = file.readline()
             return list10k
-
-
-
-    def get10klinksdates(self, cik):
-        return "10k links"
-
-    def numsecsubsecs(self, link10k):
-        print("process table of contents")
-        print( "number of headings under each section/sub-section")
-        print("num words in each sec/subsec")
-        print ('readability of each sec/sub-sec')
-
-    def numgraphs(self, link10k):
-        print ("num graphs") # graph vs pics
-        print("num pictures") #
-        print("directors and ceo numbers") # different sections for directors and ceos
-
-    def numnewwords(self, old10klink, current10klink):
-        print("num new owrds per sec/sub-sec") # what if section is not present in the previous once or vice versa
-        print("num new headings or sections")
-
